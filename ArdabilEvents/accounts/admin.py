0d23642ba70a80632a25dc97cf89bdd09724949c
EXE <bobolk1388@gmail.com>
2025-07-24 14:13:28 +0330

    Enhance admin panel for UserMetaData with custom ModelAdmin configuration

--- ArdabilEvents/accounts/admin.py
+++ ArdabilEvents/accounts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import UserMetaData
 
-admin.site.register(UserMetaData)
+@admin.register(UserMetaData)
+class UserMetaDataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'phone_number', 'email')
+    search_fields = ('user__username', 'phone_number', 'email')
+    ordering = ('user__username',)
