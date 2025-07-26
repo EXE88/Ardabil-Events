e8953dcd3ef06dcdc4696589ec075a9a6d6f4142
EXE <bobolk1388@gmail.com>
2025-07-26 14:15:15 +0330

    Add image deletion on post delete and enhance CreatePost model with description, latitude, and longitude fields

--- ArdabilEvents/posts/models.py
+++ ArdabilEvents/posts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 import uuid
+import os
 
 def upload_to(instance, filename):
     ext = filename.split('.')[-1]
@@ -20,3 +23,9 @@ class CreatePost(models.Model):
     latitude = models.FloatField(null=False,blank=False)
     longitude = models.FloatField(null=False,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+@receiver(post_delete, sender=CreatePost)
+def delete_image_file(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
